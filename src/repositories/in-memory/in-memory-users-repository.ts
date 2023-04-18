edd22338735f99110ae15962d3320177e5d0bec8
Gabriel <caampos22@gmail.com>
2023-04-17 23:45:13 -0300

    :wrench: chore: add unit test for register

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,31 @@
+import { User, Prisma } from '@prisma/client'
+import { usersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements usersRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      plan: data.plan as string,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,17 +2,28 @@ import { hash } from 'bcryptjs'
 import { usersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
-interface iRegisterUseCase {
+import type { User } from '@prisma/client'
+
+interface RegisterUseCaseRequest {
   name: string
   email: string
   plan?: string
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: usersRepository) {}
 
-  async execute({ name, email, plan, password }: iRegisterUseCase) {
+  async execute({
+    name,
+    email,
+    plan,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,11 +32,13 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       plan,
       password_hash,
     })
+
+    return { user }
   }
 }
