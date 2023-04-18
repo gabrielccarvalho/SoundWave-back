940b31c962f25795b14643edff9362ad1664542a
Gabriel <caampos22@gmail.com>
2023-04-18 15:08:39 -0300

    :sparkles: feat: add JWT verification

--- src/http/controllers/index.ts
+++ src/http/controllers/index.ts
@@ -1,2 +1,3 @@
 export { default as register } from './register'
 export { default as authenticate } from './authenticate'
+export { default as profile } from './profile'
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { register, authenticate } from './controllers'
+import { register, authenticate, profile } from './controllers'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  // Protected Routes
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
